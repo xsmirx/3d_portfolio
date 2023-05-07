bfd2699e80321483ce1070c19d02c2fa083f81fc
Ivan S <xsmirx@gmail.com>
2023-05-08 00:38:47 +0400

    fix: space of work technologies

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -38,7 +38,7 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link}) 
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag, index) => (
             <p key={tag.name} className={`text-[14px], ${tag.color}`}>
-              # {tag.name}
+              #{tag.name}
             </p>
           ))}
         </div>
